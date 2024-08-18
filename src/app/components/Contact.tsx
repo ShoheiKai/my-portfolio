a761687ab75315daebac970cce4f0e18d5f77cec
ShoheiKai <darumaya.2021st@gmail.com>
2024-08-19 02:22:15 +0900

    コンタクトフォームの修正３

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -146,7 +146,7 @@ function Contact() {
           >
             <div className="font-shipmincho font-extrabold text-xl text-main-color">
               <div className="flex flex-col md:flex-row md:justify-around items-center mb-16 md:mb-32">
-                <div className=" border-b-2 border-dark-light mx-5 md:mx-10 mb-16 md:mb-0">
+                <div className="flex flex-col border-b-2 border-dark-light mx-5 md:mx-10 mb-16 md:mb-0">
                   <label htmlFor="name" className="mr-5">
                     Name
                   </label>
@@ -158,7 +158,7 @@ function Contact() {
                     ref={nameRef}
                   />
                 </div>
-                <div className=" border-b-2 border-dark-light mx-5 md:mx-10">
+                <div className="flex flex-col border-b-2 border-dark-light mx-5 md:mx-10">
                   <label htmlFor="email" className="mr-5">
                     Email
                   </label>
