bb6810ff5be301cc2ef3813f49d60e916ec9ba58
ShoheiKai <darumaya.2021st@gmail.com>
2024-08-22 01:36:31 +0900

    MySkillSetのタグの揺れの調整

--- src/app/components/PendulumSkillSet.tsx
+++ src/app/components/PendulumSkillSet.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { motion, useAnimation, useInView } from "framer-motion";
-import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
 
 const skills = [
   "HTML/CSS",
@@ -17,32 +16,33 @@ const skills = [
   "flutter",
 ];
 
+const randomDuration = (min: number, max: number) =>
+  Math.random() * (max - min) + min;
+
+const textAnimation = {
+  skillInit: {
+    opacity: 1,
+    rotate: 0,
+  },
+  skillVisible: (index: number) => ({
+    opacity: 1,
+    rotate: [-20, 20, 0],
+    transition: {
+      duration: randomDuration(2, 5),
+      ease: "easeOut",
+    },
+  }),
+  
+};
+
 function PendulumSkillSet() {
-  const randomDuration = (min: number, max: number) =>
-    Math.random() * (max - min) + min;
-  const controls = useAnimation();
-  const ref = useRef<HTMLDivElement>(null);
-  const isInView = useInView(ref, { once: true });
-
-  useEffect(() => {
-    if (isInView) {
-      controls.start({
-        rotate: [-25, 25, 0],
-        transition: {
-          duration: randomDuration(2, 5),
-          ease: "easeInOut",
-        },
-      });
-    }
-  }, [isInView, controls]);
-
   return (
     <div className="grid grid-cols-3 md:grid-cols-6 gap-12 px-2">
       {skills.map((skill, index) => {
         return (
           <div
             key={index}
-            ref={ref}
+            // ref={ref}
             className="relative flex justify-center items-start"
           >
             {/* 押しピン画像 */}
@@ -52,7 +52,11 @@ function PendulumSkillSet() {
             <motion.div
               className="w-24 h-16 md:w-44 md:h-32 bg-slate-100 border-2 border-gray-400 rounded-md flex items-center justify-center text-gray-900 text-xs md:text-base shadow-lg"
               style={{ transformOrigin: "top center" }}
-              animate={controls}
+              // viewport={{ once: true }}
+              initial="skillInit"
+              whileInView="skillVisible"
+              variants={textAnimation}
+              custom={index}
             >
               {skill}
             </motion.div>
